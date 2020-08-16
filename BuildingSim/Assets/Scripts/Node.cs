9141eed0b2c34ddab561166792622b1dde424e86
DESKTOP-NDA8AO5\kanzaki <syo5.2525@gmail.com>
2020-08-16 23:37:35 +0900

    交差点のMeshの生成をする機構の追加

--- BuildingSim/Assets/Scripts/Node.cs
+++ BuildingSim/Assets/Scripts/Node.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -53,9 +54,9 @@ public class Node : MonoBehaviour
     {
         // Debug.Log(info.position);
         Vector3 linePos = info.bone.position;
-        Vector3 nodePos = transform.position;
+        Vector3 nodePos = position;
         var dir = Mathf.Atan2(nodePos.z - linePos.z, nodePos.x - linePos.x);
-        info.ChangeDirection(dir);
+        info.ChangeDirection(dir); 
         info.ChangeCount(_conectLineInfo.Count);
         _conectLineInfo.Add(info);
         if(_conectLineInfo.Count <= 2)
@@ -67,8 +68,9 @@ public class Node : MonoBehaviour
 
     public void ChangeLine(lineInfo info)
     {
+        Debug.Log("ChangeRoad"+ position +": "+ info.bone.position +", "+ info.lineCount);
         Vector3 linePos = info.bone.position;
-        Vector3 nodePos = transform.position;
+        Vector3 nodePos = position;
         // TODO: 一番親のBoneの角度が0になる問題の修正
         var dir = Mathf.Atan2(nodePos.z - linePos.z, nodePos.x - linePos.x);
         info.ChangeDirection(dir);
@@ -91,6 +93,7 @@ public class Node : MonoBehaviour
 
         NodesManager.Instance.AllNodeOnActive(false);
         OnActive(true);
+        // ハンドルの方向がおかしくなるので、とりあえず初期状態に戻す
         LineCreator.Instance.OnChangeHandlePos();
     }
 
@@ -131,28 +134,72 @@ public class Node : MonoBehaviour
         endRoad.SetActive(false);
     }
     
+    private List<Vector3> _intersectionVertex = new List<Vector3>();
+    
     private void SetIntersection()
     {
         transform.GetChild(0).gameObject.SetActive(false);
         _conectLineInfo.Sort(new lineInfoComparer());
-        foreach (var lineInfo in _conectLineInfo)
+        _intersectionVertex.Clear();
+        for (var i = 0; i < _conectLineInfo.Count; i++)
         {
-            //Debug.Log(lineInfo.bone.gameObject.name +": "+ lineInfo.direction);
+            //Debug.Log(_conectLineInfo[i].bone.gameObject.name +": "+ lineInfo.direction);
             // Boneの方向調整
-            var dir = lineInfo.direction * Mathf.Rad2Deg;
-            lineInfo.bone.eulerAngles = Vector3.down * dir;
+            var dir = _conectLineInfo[i].direction * Mathf.Rad2Deg;
+            _conectLineInfo[i].bone.eulerAngles = Vector3.down * dir;
             Vector3 pos = transform.position;
             pos.y = .1f;
-            lineInfo.bone.position = pos;
+            _conectLineInfo[i].bone.position = pos;
+            Vector3 linepos = new Vector3(Mathf.Cos(_conectLineInfo[i].direction + Mathf.PI), 0, Mathf.Sin(_conectLineInfo[i].direction + Mathf.PI)) + pos;
             
             // Meshの張る頂点の計算
-            
+            if (i == 0)
+            {
+                // 傾き
+                dir = _conectLineInfo[i].direction + Mathf.PI * .5f;
+                float nextDir = _conectLineInfo[_conectLineInfo.Count - 1].direction + Mathf.PI * .5f;
+                
+                // ベクトル
+                Vector3 vec = new Vector3(Mathf.Cos(dir), 0, Mathf.Sin(dir));
+                Vector3 nextVec = new Vector3(Mathf.Cos(nextDir),0,Mathf.Sin(nextDir));
+                Vector3 nextLinePos = new Vector3(Mathf.Cos(_conectLineInfo[_conectLineInfo.Count -1].direction), 0, Mathf.Sin(_conectLineInfo[_conectLineInfo.Count - 1].direction)) + pos;
+                
+                Vector3 vertex = GameManager.GetIntersection(
+                    vec * _conectLineInfo[i].lineLength + pos,
+                    vec * _conectLineInfo[i].lineLength + linepos,
+                    nextVec * -_conectLineInfo[_conectLineInfo.Count - 1].lineLength + pos,
+                    nextVec * -_conectLineInfo[_conectLineInfo.Count - 1].lineLength + nextLinePos
+                    );
+                
+                _intersectionVertex.Add(vertex - transform.position);
+            }
+            else
+            {
+                // 傾き
+                dir = _conectLineInfo[i - 1].direction + Mathf.PI * .5f;
+                float nextDir = _conectLineInfo[i].direction + Mathf.PI * .5f;
+                
+                // ベクトル
+                Vector3 vec = new Vector3(Mathf.Cos(dir), 0, Mathf.Sin(dir));
+                Vector3 nextVec = new Vector3(Mathf.Cos(nextDir),0,Mathf.Sin(nextDir));
+                Vector3 prevLinePos = new Vector3(Mathf.Cos(_conectLineInfo[i -1].direction), 0, Mathf.Sin(_conectLineInfo[i - 1].direction)) + pos;
+
+                Vector3 vertex = GameManager.GetIntersection(
+                    vec * -_conectLineInfo[i - 1].lineLength + pos,
+                    vec * -_conectLineInfo[i - 1].lineLength + prevLinePos,
+                    nextVec * _conectLineInfo[i].lineLength + pos,
+                    nextVec * _conectLineInfo[i].lineLength + linepos
+                );
+
+                _intersectionVertex.Add(vertex - transform.position);
+            }
         }
+        _meshRenderer.CreateMesh(_intersectionVertex);
     }
 
     private void OnDrawGizmos()
     {
-        Vector3 pos = transform.position;
+        Vector3 pos = position;
         Gizmos.color = Color.gray;
         if(isActive) Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(pos, .5f);
@@ -161,9 +208,19 @@ public class Node : MonoBehaviour
         {
             // Debug.Log(conectLineInfo[i].position +", "+ transform.position);
             
-            Vector3 linepos = new Vector3(Mathf.Cos(_conectLineInfo[i].direction), 0, Mathf.Sin(_conectLineInfo[i].direction)) + pos;
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(_conectLineInfo[i].bone.position, .1f);
             
+            Vector3 linepos = new Vector3(Mathf.Cos(_conectLineInfo[i].direction + Mathf.PI), 0, Mathf.Sin(_conectLineInfo[i].direction + Mathf.PI)) + pos;
+            Gizmos.color = Color.green;
             Gizmos.DrawLine(transform.position, linepos);
+            
+            if(_conectLineInfo.Count <= 2)
+                continue;
+            
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawSphere(_intersectionVertex[i] + transform.position, 0.1f);
+            
         }
     }
 }
--- BuildingSim/Assets/Scripts/NodeMeshRenderer.cs
+++ BuildingSim/Assets/Scripts/NodeMeshRenderer.cs
@@ -36,7 +36,7 @@ public class NodeMeshRenderer : MonoBehaviour
         };
         mesh.SetUVs(0,uvs);
 
-        var triangles = new List<int> {0, 1, 2, 2, 3, 0};
+        var triangles = new List<int> {0, 1, 2, 0, 2, 3};
         mesh.SetTriangles(triangles, 0);
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
@@ -44,8 +44,30 @@ public class NodeMeshRenderer : MonoBehaviour
         meshFilter.mesh = mesh;
     }
 
-    public void CreateMesh(List<Vector3> position)
+    public void CreateMesh(List<Vector3> vertex)
     {
+        mesh = new Mesh();
+
+        var uvs = new List<Vector2>();
+        var triangles = new List<int>();
+        foreach (var vert in vertex)
+        {
+            uvs.Add(new Vector2(vert.x * .1f,vert.z * .1f));
+        }
+
+        for (var i = 0; i <= vertex.Count - 2; i++)
+        {
+            triangles.Add(0);
+            triangles.Add(i + 1);
+            triangles.Add(i);
+        }
         
+        mesh.SetVertices(vertex);
+        mesh.SetUVs(0,uvs);
+        mesh.SetTriangles(triangles, 0);
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        meshFilter.mesh = mesh;
     }
 }

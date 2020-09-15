a55a061b31503047d6e81a1e1aeb98e2199d6521
DESKTOP-NDA8AO5\kanzaki <syo5.2525@gmail.com>
2020-09-16 07:28:08 +0900

    日照のサイクル追加とそれに伴うライティング設定の変更

---
+++ BuildingSim/Assets/Scripts/DayCicleController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class DayCicleController : MonoBehaviour
+{
+    [Range(0, 24)] public float timeOfDay;
+
+    public float orbitSpeed = 1.0f;
+    public Light sun;
+    public Light moon;
+
+
+    private bool isNight;
+    void Update()
+    {
+        timeOfDay += Time.deltaTime * orbitSpeed;
+        if (timeOfDay > 24)
+            timeOfDay = 0;
+        
+        UpdateTime();
+    }
+    private void OnValidate()
+    {
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        float alpha = timeOfDay / 24.0f;
+        float sunRotation = Mathf.Lerp(-90,270,alpha);
+        float moonRotation = sunRotation - 180;
+        sun.transform.rotation = Quaternion.Euler(sunRotation,-150f,0);
+        moon.transform.rotation = Quaternion.Euler(moonRotation, -150f,0);
+        
+        CheckNightDayTransition();
+    }
+
+    private void CheckNightDayTransition()
+    {
+        if (isNight)
+        {
+            if (moon.transform.rotation.eulerAngles.x > 180)
+                StartDay();
+        }
+        else
+        {
+            if (sun.transform.rotation.eulerAngles.x > 180)
+                StartNight();
+        }
+    }
+
+    private void StartDay()
+    {
+        isNight = false;
+        sun.shadows = LightShadows.Soft;
+        moon.shadows = LightShadows.None;
+    }
+
+    private void StartNight()
+    {
+        isNight = true;
+        sun.shadows = LightShadows.None;
+        moon.shadows = LightShadows.Soft;
+    }
+}
